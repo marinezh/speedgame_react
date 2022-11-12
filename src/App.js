cd3172d0fcbdd1ac32a2dfb0625f27665d4764c2
Marina <78799773+marinezh@users.noreply.github.com>
2022-11-12 23:59:52 +0200

    add speedgame react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Star from './Star'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+  stars: stars,
+}
+
+  render() {
+    return (
+      <div className="game_wrapper">
+        <h1>Speed Game</h1>
+        <h3>
+          Your score is :<span id="score">0</span>
+        </h3>
+        <div>
+        <Star />
+        </div>
+        <button>Start Game</button>
+        <button>End Game</button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Star.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Star.css";
+
+
+const Star = () => {
+  return (
+    <div className="stars">
+      <i id="star1" class="fa-solid fa-star"></i>
+    </div>
+  );
+};
+
+export default Star;
